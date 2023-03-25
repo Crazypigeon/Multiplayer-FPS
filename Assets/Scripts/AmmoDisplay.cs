6ccde86c0f7f8d37551af55cd263e51192c6b849
Justas Krikščiūnas <justas.kriksciunas@estotyvilnius.com>
2023-03-25 09:23:34 +0200

    Pataisytos kulkos

--- Assets/Scripts/AmmoDisplay.cs
+++ Assets/Scripts/AmmoDisplay.cs
@@ -7,14 +7,17 @@ public class AmmoDisplay : MonoBehaviour
 {
     [SerializeField] private Text text;
     [SerializeField] private Text text2;
-    [SerializeField] private Text text3;
+    [SerializeField] private Text reloadText1;
+    [SerializeField] private Text reloadText2;
     [SerializeField] private FpsGun ammoNumber;
 
     void Update()
     {
         text.text = Mathf.Clamp(ammoNumber.ammo, 0, 30).ToString() + "/30";
         text2.text = Mathf.Clamp(ammoNumber.ammo, 0, 30).ToString() + "/30";
-        if (ammoNumber.ammo <= 5) { text3.text = "Spausk R, kad užtaisytum ginklą"; }
-        else if (ammoNumber.ammo > 5) { text3.text = ""; }
+        if (ammoNumber.ammo <= 5) { reloadText1.text = "Spausk R, kad užtaisytum ginklą"; }
+        else if (ammoNumber.ammo > 5) { reloadText1.text = ""; }
+        if (ammoNumber.ammo <= 5) { reloadText2.text = "Spausk R, kad užtaisytum ginklą"; }
+        else if (ammoNumber.ammo > 5) { reloadText2.text = ""; }
     }
 }
